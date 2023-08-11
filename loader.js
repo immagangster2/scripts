fe0723d732547fc20fe6a299816376776a529a64
hahaha <132370261+immagangster2@users.noreply.github.com>
2023-08-11 18:30:46 +0200

    Add files via upload

---
+++ loader.js
@@ -0,0 +1,15 @@
+let files = [
+  "parseUI.js",
+  "preventReload.js",
+  "allLanguages.js",
+  "lowercaseName.js",
+  "5emotes.js",
+  "shipTag.js",
+  "explosions.js",
+  "crystalColor.js",
+  "anonMode.js",
+  "fixCustom.js",
+  "blankBadge.js",
+  "HideChat.js"
+];
+for (let file of files) executeJS("/js/main/root/algorithm/utils/main/loaders/" + file, true)
