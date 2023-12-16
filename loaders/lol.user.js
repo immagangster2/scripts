b5da85e8323601ec0c0eca80c17174d1cb2e84f6
hahaha <132370261+immagangster2@users.noreply.github.com>
2023-12-16 19:34:32 +0100

    Update lol.user.js

--- loaders/lol.user.js
+++ loaders/lol.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Troll Client CH
-// @version      1.0.1
+// @version      1.0.2
 // @description  Troll Client
 // @author       official_troller
 // @license      GPL-3.0
@@ -10,7 +10,7 @@
 // @updateURL    https://github.com/immagangster2/scripts/raw/main/loaders/lol.user.js
 // @downloadURL  https://github.com/immagangster2/scripts/raw/main/loaders/lol.user.js
 // ==/UserScript==
-const CURRENT_RUNNING_VERSION = "1.0.1";
+const CURRENT_RUNNING_VERSION = "1.0.2";
 const log = (msg) => console.log(`%c[Troll Client] ${msg}`, "color: #ffff00");
 const modlog = (msg) => console.log(`%c[Mod] ${msg}`, "color: #FF00A6");
 const stylelog = (msg) => console.log(`%c[Style] ${msg}`, "color: #06c26d");
@@ -381,11 +381,11 @@ function injectLoader() {
                 );
                 starSRC = starSRC.replace(
                     /default:t.fillStyle="hsl\(200,50%,20%\)"/
-                    , 'default:t.fillStyle="hsla(50, 100%, 50%, 0)"'
+                    , 'case"blank":t.fillStyle="hsla(50, 100%, 70%, 0)";break;default:t.fillStyle="hsl(200,50%,20%)"'
                 );
                 starSRC = starSRC.replace(
                     /default:t.fillStyle="hsl\(50,100%,70%\)"/
-                    , 'case"star":t.fillStyle="hsl(50,100%,70%)",t.fillText("S",e/2,i/2);break;default:t.fillStyle="hsla(50, 100%, 70%, 0)"'
+                    , 'case"star":t.fillStyle="hsl(50,100%,70%)",t.fillText("S",e/2,i/2);break;case"blank":t.fillStyle="hsla(50, 100%, 70%, 0)";break;default:t.fillStyle="hsl(50,100%,70%)"'
                 );
             }
             if (rabasom === "true") {
